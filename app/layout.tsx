e214a981a7f882834af7c45d42f7e7b867a0db1c
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-14 18:00:29 -0400

    chore: Add Microsoft Clarity script for website analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins, Concert_One } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import Link from "next/link";
+import Script from "next/script";
 
 const poppins = Poppins({
   weight: ["500", "600"],
@@ -47,6 +48,15 @@ export default function RootLayout({
           </div>
         </div>
       </body>
+      <Script strategy="afterInteractive" id="microsoft-clarity">
+        {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ms0m63320i");
+          `}
+      </Script>
     </html>
   );
 }
