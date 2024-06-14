7ec64e4ee2b8ec2d388d010181e998b7c9730a35
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-14 17:50:44 -0400

    chore: Update Next.js app URL to use production URL

--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,4 @@
 export const getApiUrl = (endpoint: string) => {
-  const url = process.env.NEXT_APP_URL || "http://localhost:3000";
+  const url = process.env.NEXT_APP_URL || "https://sociology-game.sohel.tech";
   return url + endpoint;
 };
